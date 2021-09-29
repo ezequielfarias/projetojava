213e5616effc310c40daee824346f9131f367795
ezequielfarias <dv.ezequielfarias@gmail.com>
2021-09-29 14:29:54 -0300

    atualizado

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("meu primeira pagina")
+var nome = "Ezequiel";
+var idade = 22;
+var idade2 = 10;
+var frase = "loucura de louco sendo louco"
+//alert(nome + " tem" + idade + "anos");//
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replase("Brasil"));
